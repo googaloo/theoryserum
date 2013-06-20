933aaa31ffd02eb7de5a3bb999bee8e21662e98f
Rusty <rustin.l.odom@gmail.com>
2013-06-20 13:12:00 -0700

    Init

---
+++ wp-content/themes/theoryserum/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body>
+	
+	<div id="page" class="hfeed site">
