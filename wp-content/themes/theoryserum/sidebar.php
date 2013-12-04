fda91169ddf0202bcdd888562d276518f27cf784
googaloo <rustin.l.odom@gmail.com>
2013-12-04 12:45:21 -0700

    Updates / styles

--- wp-content/themes/theoryserum/sidebar.php
+++ wp-content/themes/theoryserum/sidebar.php
@@ -5,6 +5,8 @@
 	<h3 class="sidebar-post-header">Books</h3>
 	<strong>Coming Soon</strong>
 	<h3 class="sidebar-post-header">Short Stories</h3>
+	<a href="<?php echo home_url(); ?>/my-short-stories/among-them/"><h4 class="my-featured-stuff-title">Among Them</h4></a>
+	<a href="<?php echo home_url(); ?>/my-short-stories/among-them/"><img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/among-them-home-thumb.jpg" class="sidebar-thumb" /></a>
 	<a href="<?php echo home_url(); ?>/my-short-stories/eye-contact/"><h4 class="my-featured-stuff-title">Eye Contact</h4></a>
 	<a href="<?php echo home_url(); ?>/my-short-stories/eye-contact/"><img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/eye-contact-home-thumb.jpg" class="sidebar-thumb" /></a>
 	<h3 class="sidebar-post-header">Games</h3>
