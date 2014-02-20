715f12ed457fe18612400b70883ef714f7a7eced
googaloo <rustin.l.odom@gmail.com>
2014-02-20 09:46:29 -0700

    update

--- wp-content/themes/theoryserum/sidebar.php
+++ wp-content/themes/theoryserum/sidebar.php
@@ -5,6 +5,8 @@
 	<h3 class="sidebar-post-header">Books</h3>
 	<strong>Coming Soon</strong>
 	<h3 class="sidebar-post-header">Short Stories</h3>
+	<a href="<?php echo home_url(); ?>/my-short-stories/plan-presented/"><h4 class="my-featured-stuff-title">A Plan Presented</h4></a>
+	<a href="<?php echo home_url(); ?>/my-short-stories/plan-presented"><img src="<?php echo home_url(); ?>/wp-content/uploads/2014/01/plan-presented-thumb.jpg" class="sidebar-thumb" /></a>
 	<a href="<?php echo home_url(); ?>/my-short-stories/among-them/"><h4 class="my-featured-stuff-title">Among Them</h4></a>
 	<a href="<?php echo home_url(); ?>/my-short-stories/among-them/"><img src="<?php echo home_url(); ?>/wp-content/uploads/2013/10/among-them-home-thumb.jpg" class="sidebar-thumb" /></a>
 	<a href="<?php echo home_url(); ?>/my-short-stories/eye-contact/"><h4 class="my-featured-stuff-title">Eye Contact</h4></a>
