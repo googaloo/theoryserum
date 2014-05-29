19d28e4bc1bb1e95249eca3292f0205b093c8a1b
googaloo <rustin.l.odom@gmail.com>
2014-05-29 07:00:09 -0700

    block quote styling, search.php

--- wp-content/themes/theoryserum2.0/search.php
+++ wp-content/themes/theoryserum2.0/search.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<h1 class="search-header">Search Results for:</h1>
-<h3><?php the_search_query(); ?></h3>
+<h1 class="page-header">Search Results for:</h1>
+<h2><?php the_search_query(); ?></h2>
 
 <?php if ( have_posts() ) : ?>
 
@@ -13,39 +13,45 @@
 
 <?php endif; ?>
 
+	<!-- break global foundation grid -->
+	</div>
+
+</div>
+
 
 <?php // PAGINATION ?>
 <?php
 if ( $prev_link = get_previous_posts_link() ) {
 	$explode_prev_link = explode('"', get_previous_posts_link());
-	$print_prev_link = '<a href="'.$explode_prev_link[1].'">Prev</a>'; 
+	$print_prev_link = '<i class="icon-arrow-left"></i><a href="'.$explode_prev_link[1].'">Prev</a>'; 
 } else {
-	$print_prev_link = "Prev";
+	$print_prev_link = "<i class='icon-arrow-left'></i>Prev";
 }
 
 if ( $next_link = get_next_posts_link() ) {
 	$explode_next_link = explode('"', get_next_posts_link());
-	$print_next_link = '<a class="ts-pagination-next" href="'.$explode_next_link[1].'">Next</a>'; 
+	$print_next_link = '<a class="ts-pagination-next" href="'.$explode_next_link[1].'">Next<i class="icon-arrow-right"></i></a>'; 
 } else {
-	$print_next_link = "Next";
+	$print_next_link = "Next<i class='icon-arrow-right'></i>";
 }
 
 ?>
 
-<nav class="row">
+<div class="ts-pagination">
 
-	<div class=" large-12 large-centered columns ts-pagination-container">
+	<div class="row">
 
-		<ul class="ts-pagination-list">
+		<div class="large-1 large-offset-9 medium-2 medium-offset-6 small-2 small-offset-6 columns"><?php echo $print_prev_link; ?></div>
+		<div class="large-1 medium-2 small-2 columns"><a href="<?php echo site_url(); ?>">Home</a></div>
+		<div class="large-1 medium-2 small-2 columns"><?php echo $print_next_link; ?></div>
 
-			<li><?php echo $print_prev_link; ?></li>
-			<li><a href="<?php echo site_url(); ?>">Home</a></li>
-			<li><?php echo $print_next_link; ?></li>
+	</div>
 
-		</ul><!-- end .ts-pagination-list -->
+</div><!-- end .ts-pagination -->
+	
+	<!-- resume global foundation grid -->
+	<div class="row">
 
-	</div><!-- end .ts-pagination-container -->
-
-</nav>
+		<div class="large-12 large-centered columns">
 
 <?php get_footer(); 
