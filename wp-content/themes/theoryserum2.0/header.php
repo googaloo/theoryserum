7fd37bdef10fe994d528653d5417a3af8e23892d
googaloo <rustin.l.odom@gmail.com>
2014-06-02 14:45:00 -0700

    update to Google Analytics

--- wp-content/themes/theoryserum2.0/header.php
+++ wp-content/themes/theoryserum2.0/header.php
@@ -17,6 +17,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
         ga('create', 'UA-42862989-1', 'theoryserum.com');
+        ga('require', 'displayfeatures');
         ga('send', 'pageview');
       </script>
 
